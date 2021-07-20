e94a47cee9fabe8d6014b13ed038b6a6d77c4fad
Gmarksa <gmaruca77@gmail.com>
2021-07-20 21:44:57 +0200

    PHP Partials

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,31 @@
 @extends("layouts.app")
 
 @section("content")
-<h1>Custom Index</h1>
+    <!--Section: Content-->
+    <section class="text-center">
+        <h4 class="mb-5"><strong>Latest posts</strong></h4>
+        
+        <div class="row">
+            @foreach($posts as $post)
+            <div class="col-lg-4 col-md-12 mb-4">
+                <div class="card" style="height: 31rem;">
+                    <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                        <img src="https://via.placeholder.com/348x232" class="img-fluid" />
+                        <a href="#!">
+                        <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+                        </a>
+                    </div>
+
+                    <div class="card-body">
+                        <h4 class="card-title" style="min-height: 50px;">{{ $post->title }}</h4>
+                        <span>By: <a href="#">{{ $post->author }}</a></span>
+                        <p class="card-text" style="min-height: 90px;">{{ $post->text }}</p>
+                        <a href="#!" class="btn btn-primary">Read</a>
+                    </div>
+                </div>
+            </div>
+            @endforeach 
+        </div>
+        
+    </section>
 @endsection 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,90 +21,13 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        <!-- Header -->
+        @include ('partials.header')
 
         <!--Main layout-->
         <main class="my-5">
             <div class="container">
-                <!--Section: Content-->
-                <section class="text-center">
-                    <h4 class="mb-5"><strong>Latest posts</strong></h4>
-                    
-                    <div class="row">
-                        @foreach($posts as $post)
-                        <div class="col-lg-4 col-md-12 mb-4">
-                            <div class="card" style="height: 31rem;">
-                                <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
-                                    <img src="https://via.placeholder.com/348x232" class="img-fluid" />
-                                    <a href="#!">
-                                    <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
-                                    </a>
-                                </div>
-
-                                <div class="card-body">
-                                    <h4 class="card-title" style="min-height: 50px;">{{ $post->title }}</h4>
-                                    <span>By: <a href="#">{{ $post->author }}</a></span>
-                                    <p class="card-text" style="min-height: 90px;">{{ $post->text }}</p>
-                                    <a href="#!" class="btn btn-primary">Read</a>
-                                </div>
-                            </div>
-                        </div>
-                        @endforeach 
-                    </div>
-           
-                    
-
-                    
-                </section>
+                @yield("content")
 
                 <!-- Pagination -->
                 <nav class="my-4" aria-label="...">
