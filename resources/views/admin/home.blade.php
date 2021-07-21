0d292649242e0ee2248e08d6ef25d03b06f83bc4
Gmarksa <gmaruca77@gmail.com>
2021-07-21 15:32:20 +0200

    Change Home For Admin Panel

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -4,8 +4,8 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+            <div class="card mb-3">
+                <div class="card-header">{{ __('Welcome back!') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -15,8 +15,21 @@
                     @endif
 
                     {{ __('You are logged in!') }}
+
+
                 </div>
             </div>
+
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    <div>
+                        <a href="admin/posts"><h4>Manage Posts</h4></a>
+                    </div>
+                </div>
+            </div>
+
         </div>
     </div>
 </div>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -20,7 +20,7 @@
                         <h4 class="card-title" style="min-height: 50px;">{{ $post->title }}</h4>
                         <span>By: <a href="#">{{ $post->author }}</a></span>
                         <p class="card-text" style="min-height: 90px;">{{ $post->text }}</p>
-                        <a hreF="{{ route("posts.show", $post->id) }}" class="btn btn-primary">Read</a>
+                        <a hreF="{{ route("posts.show", $post->id) }}" class="btn btn-primary w-100">Read</a>
                     </div>
                 </div>
             </div>
