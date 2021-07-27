ffa086a312f1e5f5c93f97dd45d00d5844b200f8
Gmarksa <gmaruca77@gmail.com>
2021-07-27 20:17:20 +0200

    Removed BelongsTo From Category Model

--- app/Category.php
+++ app/Category.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    public function posts(): BelongsTo
+    public function posts()
     {
         return $this->hasMany(Post::class);
     }
